4f4bcc3c70c711c2800395b2a15a6890b0c25ba1
Ian Tyler Applebaum <Ian.tyler@electricbluefish.com>
2020-09-23 20:19:07 -0400

    removed unnecessary space

--- Sources/Markdownify/Markdownify.swift
+++ Sources/Markdownify/Markdownify.swift
@@ -3,17 +3,10 @@ import PythonKit
 struct Markdownify {
 	func checkPython() -> String{
 		let sys = try Python.import("sys")
-
 		return "Python \(sys.version_info.major).\(sys.version_info.minor)"
-		
-		
 	}
 	func convertHTMLtoMarkdown(_ html: String) -> String? {
-	
 		let md = try Python.import("markdownify")
-		
-		
-	
 		return md.markdownify(html).description
 	}
 }
